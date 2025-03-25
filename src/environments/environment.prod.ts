f82f32288ccc741c24f16e80573c2114913b97c2
Adewunmi <adewunmi7576@gmail.com>
2025-03-25 02:34:02 +0100

    new new env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://4c10-105-112-204-173.ngrok-free.app/'
+    apiUrl: 'https://4c10-105-112-204-173.ngrok-free.app'
   };
   
