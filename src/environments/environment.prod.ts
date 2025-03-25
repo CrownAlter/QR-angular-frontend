1ec051c31a77a7fc4540edbfc71a7a5ab4bc1004
Adewunmi <adewunmi7576@gmail.com>
2025-03-25 11:05:46 +0100

    defense test 8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'http://147.185.221.17:55816'
+    apiUrl: 'https://qr-auth-backend-iv1n.onrender.com'
   };
   
