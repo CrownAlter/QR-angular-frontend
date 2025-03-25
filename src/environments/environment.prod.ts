b76de024440c7d0ed9235673b1ad90c94cc71636
Adewunmi <adewunmi7576@gmail.com>
2025-03-25 03:26:04 +0100

    localtunnel env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://f709-105-112-204-173.ngrok-free.app'
+    apiUrl: 'https://good-pigs-wear.loca.lt'
   };
   
