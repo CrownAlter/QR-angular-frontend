797f13c087afef776a5653738b3111595a32a0e5
Adewunmi <adewunmi7576@gmail.com>
2025-03-25 09:24:20 +0100

    defense test 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://shaggy-foxes-check.loca.lt/'
+    apiUrl: 'https://wide-beds-tap.loca.lt'
   };
   
